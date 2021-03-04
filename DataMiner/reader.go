9133e825dc1709cf4d62344287889a454727f2ba
cbroz-tgm <cbroz@student.tgm.ac.at>
2021-03-04 13:43:28 +0100

    Funktion für Filetransfer

--- DataMiner/reader.go
+++ DataMiner/reader.go
@@ -1,9 +1,50 @@
-package DataMiner
+package main
 
 import "fmt"
+import "io/ioutil"
+import "os"
 
-func
+var observed_dir = "C:/Users/chris/OneDrive/Dokumente/Diplomarbeit/ReWriteGo/csv-files"
+
+func transferFiles() {
+
+	//------------------------------------------------------------------
+	// Dir aus dem CSV-Files bezogen werden
+	files, error := ioutil.ReadDir(observed_dir)
+	if error != nil {
+		fmt.Println("Error reading directory  \n")
+		fmt.Println(error)
+	}
+	//------------------------------------------------------------------
+
+	// Jedes File lesen und in neues Dir kopieren
+
+	for _, file := range files {
+		fmt.Println(file.Name() + "\n")
+		data, error := ioutil.ReadFile(observed_dir + "/" + file.Name())
+		if error != nil {
+			fmt.Println("Error reading file " + file.Name() + "\n")
+			fmt.Println(error)
+		}
+
+		error = os.Remove(observed_dir + "/" + file.Name())
+		if error != nil {
+			//log.Fatal(error)
+			fmt.Println("Error deleting file " + file.Name() + "\n")
+			fmt.Println(error)
+		}
+
+		error = ioutil.WriteFile("DataMiner/input/"+file.Name(), data, 0644)
+		if error != nil {
+			fmt.Println("Error writing file " + file.Name() + "\n")
+			fmt.Println(error)
+		}
+
+	}
+
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	transferFiles()
+	fmt.Println("Completed Filetransfer!")
 }
