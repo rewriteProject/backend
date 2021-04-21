4529b62505052093238be98685d408a9774c7292
cbroz-tgm <cbroz@student.tgm.ac.at>
2021-04-21 10:21:41 +0200

    Statistik und Prognose P2 geupdatet

--- DataAnalysis/QueryManager.go
+++ DataAnalysis/QueryManager.go
@@ -295,22 +295,89 @@ func StatisticHandler(w http.ResponseWriter, r *http.Request) {
 	defer dbConn.Close()
 
 	// Abrufen der Pfad Parameter
-	//parameters := mux.Vars(r)
-	//country := parameters["country"]
-	//attribute := parameters["attribute"]
-	//attributes_list := strings.Split(attribute, ",")
-	//minDate, _ := r.URL.Query()["minDate"]
-	//maxDate, _ := r.URL.Query()["maxDate"]
+	parameters := mux.Vars(r)
+	country := parameters["country"]
+	attributes := parameters["attributes"]
+	attributes_list := strings.Split(attributes, ",")
+	minDate, _ := r.URL.Query()["minDate"]
+	maxDate, _ := r.URL.Query()["maxDate"]
 
 	//Schickt die Query an die Datenbank
-	results, error := dbConn.Query("Select * from customer")
-	if error != nil {
-		panic(error.Error())
+	//results, error := dbConn.Query("Select * from customer")
+	//if error != nil {
+	//	panic(error.Error())
+	//}
+	//defer results.Close()
+	nestedjson := gabs.New()
+	_, _ = nestedjson.Set("CLOSE", "container", "status")
+	_, _ = nestedjson.Set(minDate[0], "container", "start_time")
+	_, _ = nestedjson.Set(maxDate[0], "container", "end_time")
+	//_, _ = nestedjson.SetP("", "container.country."+country)
+	fmt.Println(attributes)
+	fmt.Println(attributes_list)
+	jsonMap := map[string]interface{}{}
+
+	if checkForDoubleElement(attributes_list, "color") == true {
+		jsonMap = map[string]interface{}{
+			"red":   2,
+			"blue":  5,
+			"green": 10}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"color")
+		_, _ = nestedjson.Set("color", "container", "type")
 	}
-	defer results.Close()
+	if checkForDoubleElement(attributes_list, "brand") == true {
+		jsonMap = map[string]interface{}{
+			"nike":   7,
+			"adidas": 3,
+			"hm":     7}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"brand")
+		_, _ = nestedjson.Set("brand", "container", "type")
+	}
+	if checkForDoubleElement(attributes_list, "category") == true {
+		jsonMap = map[string]interface{}{
+			"t-shirt": 6,
+			"shoes":   9,
+			"pants":   2}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"category")
+		_, _ = nestedjson.Set("category", "container", "type")
+	}
+	if checkForDoubleElement(attributes_list, "all") == true {
+		jsonMap = map[string]interface{}{
+			"red":   2,
+			"blue":  5,
+			"green": 10}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"color")
+		jsonMap = map[string]interface{}{
+			"nike":   7,
+			"adidas": 3,
+			"hm":     7}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"brand")
+		jsonMap = map[string]interface{}{
+			"t-shirt": 6,
+			"shoes":   9,
+			"pants":   2}
+		_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"category")
+		_, _ = nestedjson.Set("", "container", "type")
+	}
+	//jsonMap = map[string]interface{}{
+	//	"red": 2,
+	//	"blue": 5,
+	//	"green": 10}
+	//_, _ = nestedjson.SetP(jsonMap, "container.country."+country+"."+"color")
+	json.NewEncoder(w).Encode(nestedjson)
+
 }
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------
+type P2 struct {
+	id_bill      int
+	id_container int
+	close_date   string
+	color        string
+	brand        string
+	category     string
+}
+
 // Prognose
 func ForecastHandler(w http.ResponseWriter, r *http.Request) {
 	dbConn, error := sql.Open("mysql", "root:wasd123@tcp(127.0.0.1:3306)/logistics")
@@ -324,7 +391,7 @@ func ForecastHandler(w http.ResponseWriter, r *http.Request) {
 	Case := parameters["Case"]
 	country := parameters["country"]
 	minDate, _ := r.URL.Query()["minDate"]
-	year, _ := r.URL.Query()["year"]
+	//year, _ := r.URL.Query()["year"]
 	typ, _ := r.URL.Query()["typ"]
 	feature, _ := r.URL.Query()["feature"]
 
@@ -392,36 +459,118 @@ func ForecastHandler(w http.ResponseWriter, r *http.Request) {
 
 	} else if Case == "p2" {
 		// Zu welchem Monat ein Container gezählt wird hängt vom Closedate ab
-		query := "select bill.id_bill, container.id_container, container.to_country, container.close_date, product.color, product.brand, product.category from container inner join bill on bill.id_container=container.id_container right join product on bill.id_bill=product.id_bill where status='0' and to_country='" + country + "' and year(close_date)='" + year[0] + "'"
-		results, error := dbConn.Query(query)
-		if error != nil {
-			panic(error.Error())
-		}
-		defer results.Close()
+		//query := "select bill.id_bill, container.id_container, container.to_country, container.close_date, product.color, product.brand, product.category from container inner join bill on bill.id_container=container.id_container right join product on bill.id_bill=product.id_bill where status='0' and to_country='" + country + "' and close_date between '" + minDate[0] + "' and now()"
+		//results, error := dbConn.Query(query)
+		//if error != nil {
+		//	panic(error.Error())
+		//}
+		//defer results.Close()
 
 		//Gabs Package für erweitere json Strukturen
 		nestedjson := gabs.New()
 		_, _ = nestedjson.Set("CLOSE", "container", "status")
 		_, _ = nestedjson.Set(country, "container", "country")
 		_, _ = nestedjson.Set(typ[0], "container", "type")
-		_, _ = nestedjson.Set(year[0], "container", "year")
+		_, _ = nestedjson.Set(minDate[0], "container", "min_date")
 		_, _ = nestedjson.Set("m", "container", "intervall")
 
 		// Für jeden Monat die Anzahl an Einträgen für gegebenes Merkmal (color, brand, category)
-		_, _ = nestedjson.Set(feature[0], "container", "intervall")
+		//_, _ = nestedjson.Set(feature[0], "container", "intervall")
+
+		//list := make([]string, 0)
+		//jsonList := make([]string, 0)
 		//for results.Next() {
-		//	var product Product
-		//	error = results.Scan(&product.color, &product.brand, &product.category)
+		//	var p2 P2
+		//	error = results.Scan(&p2.close_date)
 		//	if error != nil {
 		//		panic(error.Error())
 		//	}
+		//	//list // append trimmed Date
 		//
-		//	jsonMap := map[string]interface{}{
-		//		"01":container.create_date,
-		//		"close_date":container.close_date}
-		//	_, _ = nestedjson.SetP(jsonMap, "container."+typ[0]+"."+feature[0])
 		//}
 
+		/*
+			Zusätzliche Schleife für Datumsliste
+				Überprüfen ob gleiches Datum, wenn ja dann steigt counter
+		*/
+
+		jsonMap := map[string]interface{}{
+			"01-2015": 56,
+			"02-2015": 47,
+			"03-2015": 87,
+			"04-2015": 101,
+			"05-2015": 168,
+			"06-2015": 216,
+			"07-2015": 231,
+			"08-2015": 174,
+			"09-2015": 168,
+			"10-2015": 98,
+			"11-2015": 65,
+			"12-2015": 60,
+			"01-2016": 41,
+			"02-2016": 49,
+			"03-2016": 71,
+			"04-2016": 92,
+			"05-2016": 156,
+			"06-2016": 189,
+			"07-2016": 198,
+			"08-2016": 145,
+			"09-2016": 101,
+			"10-2016": 86,
+			"11-2016": 57,
+			"12-2016": 39,
+			"01-2017": 42,
+			"02-2017": 53,
+			"03-2017": 65,
+			"04-2017": 86,
+			"05-2017": 91,
+			"06-2017": 130,
+			"07-2017": 156,
+			"08-2017": 148,
+			"09-2017": 132,
+			"10-2017": 98,
+			"11-2017": 85,
+			"12-2017": 70,
+			"01-2018": 73,
+			"02-2018": 81,
+			"03-2018": 88,
+			"04-2018": 107,
+			"05-2018": 136,
+			"06-2018": 187,
+			"07-2018": 212,
+			"08-2018": 199,
+			"09-2018": 156,
+			"10-2018": 121,
+			"11-2018": 99,
+			"12-2018": 87,
+			"01-2019": 71,
+			"02-2019": 81,
+			"03-2019": 109,
+			"04-2019": 122,
+			"05-2019": 158,
+			"06-2019": 191,
+			"07-2019": 220,
+			"08-2019": 225,
+			"09-2019": 198,
+			"10-2019": 156,
+			"11-2019": 98,
+			"12-2019": 89,
+			"01-2020": 92,
+			"02-2020": 81,
+			"03-2020": 78,
+			"04-2020": 54,
+			"05-2020": 61,
+			"06-2020": 98,
+			"07-2020": 121,
+			"08-2020": 118,
+			"09-2020": 101,
+			"10-2020": 81,
+			"11-2020": 47,
+			"12-2020": 31,
+			"01-2021": 33,
+			"02-2021": 48,
+			"03-2021": 56}
+		_, _ = nestedjson.SetP(jsonMap, "container."+typ[0]+"."+feature[0])
 		json.NewEncoder(w).Encode(nestedjson)
 	}
 }
